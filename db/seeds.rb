8bc1880de2abe5282fb413c3804281b01b33ab17
Brian Cortes <briancorts166@gmail.com>
2018-12-01 18:52:48 -0500

    added User and Messages resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Brian', password: 'password')
+User.create(username: 'Obi Wan', password: 'password')
+User.create(username: 'Ninja', password: 'password')
+User.create(username: 'Logan', password: 'password')
+User.create(username: 'Pewdiepie', password: 'password')
+
+Message.create(body: 'Hi there!', user: User.first)
+Message.create(body: 'yo!', user: User.second)
+Message.create(body: 'howdy!', user: User.third)
+Message.create(body: 'bazinga!', user: User.fourth)
+Message.create(body: 'wtf!', user: User.first)
